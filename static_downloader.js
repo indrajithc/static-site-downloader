9c8f13a6cedeb0645da118a61552e260d8397887
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2025-05-22 14:31:40 +0530

    refactor: ssl disabled

--- static_downloader.js
+++ static_downloader.js
@@ -4,17 +4,31 @@ const fs = require("fs-extra");
 const path = require("path");
 const { URL } = require("url");
 const express = require("express");
+const https = require('https');
+
 
 const STATIC_DIR = path.join(__dirname, "static_server_folder");
 const LOCAL_SERVER_PORT = 3000;
 const LOCAL_SERVER_HOST = `http://localhost:${LOCAL_SERVER_PORT}`;
+
+const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+
 
 async function downloadWebsite(targetUrl) {
   // Step 0: Clean up old folder
   await fs.remove(STATIC_DIR);
   await fs.ensureDir(STATIC_DIR);
 
-  const { data: html } = await axios.get(targetUrl);
+  const { data: html } = await axios.get(targetUrl, {
+    httpsAgent,
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+    },
+  });
+  if (!html) {
+    throw new Error("Failed to fetch HTML content.");
+  }
   const $ = cheerio.load(html);
   const baseUrl = new URL(targetUrl);
 
@@ -37,7 +51,16 @@ async function downloadWebsite(targetUrl) {
     const localCssPath = path.join(STATIC_DIR, cssPath);
 
     try {
-      const { data: cssContent } = await axios.get(cssUrl);
+      const { data: cssContent } = await axios.get(cssUrl, {
+        httpsAgent,
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+        },
+      });
+      if (!cssContent) {
+        throw new Error("Failed to fetch CSS content.");
+      }
       await fs.ensureDir(path.dirname(localCssPath));
       await fs.writeFile(localCssPath, cssContent, "utf-8");
 
